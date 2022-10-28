07a15645fc8d5968cfa85350926eddb33e871b09
KayTeo <keith_teo@outlook.sg>
2022-10-28 13:15:53 +0800

    Added basic things

--- Knapsack/knapsack.cpp
+++ Knapsack/knapsack.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
+//Capacity
+vector<int> weight;
+vector<int> profit;
+
+//wIndex and pIndex tracks last element of knapsack vectors
+int knapsack(int wIndex, int pIndex, int weightStart, int profitStart){
+    //TODO: Write base case
+
+    //TODO: Finish recursion
+    int ans = max(knapsack(wIndex))
+}
+
 int main(){
+
+    int c = 100; 
+
+
+
     return 0;
 }
